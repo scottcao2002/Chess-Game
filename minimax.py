e1d4a27db0ffc7ad79c225d80a02ed39ec1b4741
scottcao2002 <53843120+scottcao2002@users.noreply.github.com>
2020-08-11 15:04:10 -0700

    Create minimax.py

---
+++ minimax.py
@@ -0,0 +1,43 @@
+#minimax algorithm used for the AI
+def minimax(depth, alpha, beta, isComputer):
+    global PPlayer
+    global POpponent
+    global chessboard
+    if depth == 0 or abs(POpponent.getScore()) == maxsize or POpponent.canMove() == 0 or PPlayer.canMove() == 0:
+        return POpponent.getScore()
+    if isComputer:
+        for piece in POpponent.getAllPieces():
+            if type(piece) is Pawn:
+                piece.tempMovedTwo = False
+        bestScore = -maxsize - 1
+        for piece in POpponent.getAllPieces():
+            if chessboard[piece.getRow()][piece.getColumn()].piece == piece.ID:
+                if len(piece.getPossibilities()) > 0:
+                    for possibility in piece.getPossibilities():
+                        piece.tempMove(possibility[0], possibility[1])
+                        score = minimax(depth-1, alpha, beta, False)
+                        piece.moveBack()
+                        bestScore = max(score, bestScore)
+                        alpha = max(alpha, score)
+                        if beta <= alpha:
+                            break
+                    
+        return bestScore
+    else:
+        for piece in PPlayer.getAllPieces():
+            if type(piece) is Pawn:
+                piece.tempMovedTwo = False
+        bestScore = maxsize + 1
+        for piece in PPlayer.getAllPieces():
+            if chessboard[piece.getRow()][piece.getColumn()].piece == piece.ID:
+                if len(piece.getPossibilities()) > 0:
+                    for possibility in piece.getPossibilities():
+                        piece.tempMove(possibility[0], possibility[1])
+                        score = minimax(depth - 1, alpha, beta, True)
+                        piece.moveBack()
+                        bestScore = min(score, bestScore)
+                        beta = min(score, beta)
+                        if beta <= alpha:
+                            break
+        return bestScore
+                
