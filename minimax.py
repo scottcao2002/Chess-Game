e634d1d56baa0e1f5bf70a6fcd06494b2ee74bd3
scottcao2002 <53843120+scottcao2002@users.noreply.github.com>
2020-08-12 10:47:53 -0700

    Update minimax.py

--- minimax.py
+++ minimax.py
@@ -3,6 +3,8 @@ def minimax(depth, alpha, beta, isComputer):
     global PPlayer
     global POpponent
     global chessboard
+    global tempOpponentTurn
+    global tempPlayerTurn
     if depth == 0 or abs(POpponent.getScore()) == maxsize or POpponent.canMove() == 0 or PPlayer.canMove() == 0:
         return POpponent.getScore()
     if isComputer:
@@ -15,8 +17,12 @@ def minimax(depth, alpha, beta, isComputer):
                 if len(piece.getPossibilities()) > 0:
                     for possibility in piece.getPossibilities():
                         piece.tempMove(possibility[0], possibility[1])
+                        tempPlayerTurn = True
+                        tempOpponentTurn = False
                         score = minimax(depth-1, alpha, beta, False)
                         piece.moveBack()
+                        tempPlayerTurn = False
+                        tempOpponentTurn = True
                         bestScore = max(score, bestScore)
                         alpha = max(alpha, score)
                         if beta <= alpha:
